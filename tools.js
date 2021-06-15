ff60687549e4d48dff424760c512584b85e6e560
wikgu DEV <48187208+wikgu@users.noreply.github.com>
2021-06-16 01:25:56 +0200

    Update tools.js

--- tools.js
+++ tools.js
@@ -2,8 +2,21 @@ let data = {
     config: require('./config.json'),
     colors: require('./colors.js')
 }
-
+/**
+ * @module Tools
+ * @name Tools
+ * @description Script toolkit
+ * @method log Log message to console
+ */
 function Tools() {
+    /**
+     * Console logger
+     * @param {Number} level Level of log (lower = more important)
+     * @param {String} message Message to show
+     * @param {String} foreground Foreground color from colors.js
+     * @param {String} background Background color from colors.js
+     * @returns response data
+     */
     this.log = (level, message, foreground, background) => {
         let reject = (reason) => {
             response.rejected = reason;
@@ -12,6 +25,8 @@ function Tools() {
         let response = {
             level,
             message,
+            foreground,
+            background,
             date: new Date().toUTCString()
         }
         if(data.config.debug < level) return reject("Lower debug level (config.json)");
@@ -21,7 +36,8 @@ function Tools() {
             fg: data.colors[foreground]||data.colors.fg.green,
             bg: data.colors[background]||data.colors.bg.black
         };
-        console.log(`${response.date} | ${data.config.name} > ${colors.fg}${colors.bg}${message}`);
+        console.log(`${response.date} | ${data.colors.bright}${data.config.name} ${data.colors.reset}> ${colors.fg}${colors.bg}${message}`);
+        return response;
     }
 }
 module.exports = Tools;
