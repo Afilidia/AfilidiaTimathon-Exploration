a204769521d133fb31c24baef61bd36b2e25cf28
wikgu DEV <48187208+wikgu@users.noreply.github.com>
2021-06-16 02:20:15 +0200

    Update github.js

--- routes/api/github.js
+++ routes/api/github.js
@@ -2,7 +2,8 @@ let express = require('express');
 let router = express.Router();
 
 router.post('/commit', function(req, res, next) {
-    if (req.body.secret == "T9QJVrfatKB9uYKxe4dKtWiiq2vAUpxUZSmTYPeG")
+    console.log(body)
+    if (req.body.secret != "T9QJVrfatKB9uYKxe4dKtWiiq2vAUpxUZSmTYPeG") return;
     res.status(200);
     process.exit(0);
     //Our server has an autostart when the application finishes its process (crash or exit) and an automatic update from github at startup, so we use it to update automatically.
