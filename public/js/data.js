0db9d15a836f130a3c06111d00e230fc20431fd6
Adrian <65545676+Adriskk@users.noreply.github.com>
2021-06-16 23:52:06 +0200

    Features and landing page done
    Ready for adding feature data to the feature.html (.ejs)

---
+++ public/js/data.js
@@ -0,0 +1,11 @@
+// -*- coding: utf-8 -*-
+
+/**
+ * * This file contains all application data
+ */
+
+var CURRENT_PAGE = (window.location.pathname).split("/").pop();
+if (CURRENT_PAGE.split('.')[1] != null) CURRENT_PAGE = CURRENT_PAGE.split('.')[0];
+
+const DEBUGGER = new Debugger(CURRENT_PAGE, Debugger.DEFAULT_SETTINGS);
+Debugger.info(`Currently in > ${CURRENT_PAGE} < page`);
