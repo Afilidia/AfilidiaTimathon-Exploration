8efea4f7ec3fc4c659dc2d37fd9553ddf675bd68
wikgu DEV <48187208+wikgu@users.noreply.github.com>
2021-06-16 02:38:28 +0200

    Update github.js

--- routes/api/github.js
+++ routes/api/github.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let router = express.Router();
+let crypto = require('crypto');
 
 router.post('/commit', function(req, res, next) {
-    console.log(body)
-    res.status(200);
+    if(req.header['x-hub-signature'] != "sha1=" + crypto.createHmac('sha1', process.env.githubsecret||"T9QJVrfatKB9uYKxe4dKtWiiq2vAUpxUZSmTYPeG").update(chunk.toString()).digest('hex')) return res.statusCode(200);
+    res.statusCode(200);
     process.exit(0);
     //Our server has an autostart when the application finishes its process (crash or exit) and an automatic update from github at startup, so we use it to update automatically.
 });
