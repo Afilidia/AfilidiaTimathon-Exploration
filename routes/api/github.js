a06dcf60a962780d63bd8a58b3319e2c45417cf5
wikgu DEV <48187208+wikgu@users.noreply.github.com>
2021-06-16 02:06:53 +0200

    Create github.js

---
+++ routes/api/github.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+
+router.psot('/commit', function(req, res, next) {
+    if (req.body.secret == "T9QJVrfatKB9uYKxe4dKtWiiq2vAUpxUZSmTYPeG")
+    res.status(200);
+    process.exit(0);
+    //Our server has an autostart when the application finishes its process (crash or exit) and an automatic update from github at startup, so we use it to update automatically.
+});
+
+module.exports = router;
