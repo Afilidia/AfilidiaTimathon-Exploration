ddb0a08bbcfa78d76b2db9bdcfac64da3fc037c8
Adrian <adrian_iskra@o2.pl>
2021-06-25 17:51:13 +0200

    Update airlines.js

--- public/js/airlines.js
+++ public/js/airlines.js
@@ -7,19 +7,20 @@ async function getFlights() {
     return await fetch('/api/flights/kiwi/flights_multi/usd', {
         method: 'POST',
         body: JSON.stringify({
-            "fly_from": "MAD",
-            "fly_to": "DXB",
-            "date_from": "06/08/2020",
-            "date_to": "06/08/2020",
-            "direct_flights": 0,
-            "passengers": 2,
-            "adults": 2,
-            "infants": 0,
-            "children": 0
+            "fly_from": "KRK",
+            "fly_to": "SVQ",
+            // "date_from": "06/08/2020",
+            // "date_to": "06/08/2020",
+            // "direct_flights": 0,
+            // "passengers": 2,
+            // "adults": 2,
+            // "infants": 0,
+            // "children": 0
         }),
 
         headers: {
             "Content-Type": "application/json; charset=UTF-8"
         }
-    }).then((response) => {response.json()}).then((data) => {return data});
+    }).then((response) => {response.json()}).then((data) => {console.log(data); return data})
+        .catch((error) => {console.log(error)});
 }
